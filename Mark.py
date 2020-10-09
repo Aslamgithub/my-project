7b38110255ba11c805ff2ccaa7f5e3cf50553c7a
Aslamgithub <72614275+Aslamgithub@users.noreply.github.com>
2020-10-10 00:29:43 +0530

    Initial Marks

---
+++ Mark.py
@@ -0,0 +1,10 @@
+print("subject total ,if function")
+print('......................................................................')
+A= int(input ("enter Maths marks:"))
+B= int(input ("enter Tamil marks:"))
+C= int(input ("enter Arabic marks:"))
+Total=(A+B+C)
+Average=(A+B+C)/3
+print "Total=",Total
+print "Average=",Average
+
