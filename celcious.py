ce9442f901a2a40db181d003bec54c4c6041e4f5
athithan176005H <72656537+athithan176005H@users.noreply.github.com>
2020-10-10 19:34:15 +0530

    correcting equation error
    42 should be 32 in the equation

--- celcious.py
+++ celcious.py
@@ -1,5 +1,5 @@
 print("Temperature")
 print('......................................................................')
 L=int(input("Enter the celcious value:"))
-M=L*9.0/5.0+42
+M=L*9.0/5.0+32
 print("Temperature in Farenheit:"),M,"F"
