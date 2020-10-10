8d78a8492cb7c289e2857ce78e460f6ff37ef40e
Aslamgithub <72614275+Aslamgithub@users.noreply.github.com>
2020-10-10 19:29:54 +0530

    celcoios farheneit converter

---
+++ celcious.py
@@ -0,0 +1,5 @@
+print("Temperature")
+print('......................................................................')
+L=int(input("Enter the celcious value:"))
+M=L*9.0/5.0+42
+print("Temperature in Farenheit:"),M,"F"
